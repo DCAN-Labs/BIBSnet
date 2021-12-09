1f5653b38fe9e994f20b6cec84a8c0a431738d0e
paul-reiners <paul.reiners@gmail.com>
2021-12-09 13:11:43 -0600

    Factor out common code into a method.

--- src/img_processing/test_correct_chirality.py
+++ src/img_processing/test_correct_chirality.py
@@ -49,15 +49,37 @@ def test_correct_chirality_correct_right_label():
                       nifti_output_file_path)
     correctly_labeled_right = (60, 96, 96)
     correct_right_label = 'Right-Cerebral-White-Matter'
+    test_correct_labels(correct_right_label, correctly_labeled_right, nifti_input_file_path, nifti_output_file_path,
+                        segment_lookup_table)
+
+
+def test_correct_labels(correct_label, correctly_labeled, nifti_input_file_path, nifti_output_file_path,
+                        segment_lookup_table):
     input_data = get_input_data(nifti_input_file_path)
     free_surfer_label_to_region = get_id_to_region_mapping(segment_lookup_table)
     output_data = get_output_data(nifti_output_file_path)
-    assert correct_right_label == free_surfer_label_to_region[
-            input_data[correctly_labeled_right[0]][correctly_labeled_right[1]][correctly_labeled_right[2]]]
-    assert correct_right_label == free_surfer_label_to_region[
-            output_data[correctly_labeled_right[0]][correctly_labeled_right[1]][correctly_labeled_right[2]]]
+    assert correct_label == free_surfer_label_to_region[
+        input_data[correctly_labeled[0]][correctly_labeled[1]][correctly_labeled[2]]]
+    assert correct_label == free_surfer_label_to_region[
+        output_data[correctly_labeled[0]][correctly_labeled[1]][correctly_labeled[2]]]
+
+
+def test_correct_chirality_correct_left_label():
+    nifti_input_file_path = \
+        '../../temp/test_subject_data/1mo/sub-00006_ses-20170806_nnUnet_aseg_pre_chircorrection.nii.gz'
+    segment_lookup_table = '../../data/look_up_tables/FreeSurferColorLUT.txt'
+    left_right_mask_nifti_file = '../../temp/test_subject_data/1mo/LRmask.nii.gz'
+    fp = tempfile.NamedTemporaryFile(suffix='.nii.gz')
+    nifti_output_file_path = fp.name
+    correct_chirality(nifti_input_file_path, segment_lookup_table, left_right_mask_nifti_file,
+                      nifti_output_file_path)
+    correctly_labeled_left = (109, 96, 90)
+    correct_left_label = 'Left-Cerebral-White-Matter'
+    test_correct_labels(correct_left_label, correctly_labeled_left, nifti_input_file_path, nifti_output_file_path,
+                        segment_lookup_table)
 
 
 if __name__ == "__main__":
     test_correct_chirality_incorrect_right_label()
     test_correct_chirality_correct_right_label()
+    test_correct_chirality_correct_left_label()
