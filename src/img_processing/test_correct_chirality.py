716bfc4fee345fda4d04c7e2252a1483de74d8c5
paul-reiners <paul.reiners@gmail.com>
2021-12-09 11:15:17 -0600

    Get rid of duplicate code.

--- src/img_processing/test_correct_chirality.py
+++ src/img_processing/test_correct_chirality.py
@@ -16,16 +16,26 @@ def test_correct_chirality_incorrect_right_label():
                       nifti_output_file_path)
     incorrectly_labeled_left = (62, 91, 91)
     incorrect_right_label = 'Left-Cerebral-White-Matter'
-    input_img = nib.load(nifti_input_file_path)
-    input_data = input_img.get_data()
+    input_data = get_input_data(nifti_input_file_path)
     free_surfer_label_to_region = get_id_to_region_mapping(segment_lookup_table)
-    output_img = nib.load(nifti_output_file_path)
-    output_data = output_img.get_data()
+    output_data = get_output_data(nifti_output_file_path)
     assert incorrect_right_label == free_surfer_label_to_region[
             input_data[incorrectly_labeled_left[0]][incorrectly_labeled_left[1]][incorrectly_labeled_left[2]]]
     correct_right_label = 'Right-Cerebral-White-Matter'
     assert correct_right_label == free_surfer_label_to_region[
             output_data[incorrectly_labeled_left[0]][incorrectly_labeled_left[1]][incorrectly_labeled_left[2]]]
+
+
+def get_output_data(nifti_output_file_path):
+    output_img = nib.load(nifti_output_file_path)
+    output_data = output_img.get_data()
+    return output_data
+
+
+def get_input_data(nifti_input_file_path):
+    input_img = nib.load(nifti_input_file_path)
+    input_data = input_img.get_data()
+    return input_data
 
 
 def test_correct_chirality_correct_right_label():
@@ -39,11 +49,9 @@ def test_correct_chirality_correct_right_label():
                       nifti_output_file_path)
     correctly_labeled_right = (60, 96, 96)
     correct_right_label = 'Right-Cerebral-White-Matter'
-    input_img = nib.load(nifti_input_file_path)
-    input_data = input_img.get_data()
+    input_data = get_input_data(nifti_input_file_path)
     free_surfer_label_to_region = get_id_to_region_mapping(segment_lookup_table)
-    output_img = nib.load(nifti_output_file_path)
-    output_data = output_img.get_data()
+    output_data = get_output_data(nifti_output_file_path)
     assert correct_right_label == free_surfer_label_to_region[
             input_data[correctly_labeled_right[0]][correctly_labeled_right[1]][correctly_labeled_right[2]]]
     assert correct_right_label == free_surfer_label_to_region[
