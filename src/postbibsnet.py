c63bbc4ec40b8cc621a94eb59c8caeb99c0ff175
Barry Tikalsky <tikal004@umn.edu>
2023-10-13 10:55:16 -0500

    adding remove_extra_clusters_from_mask

--- src/postbibsnet.py
+++ src/postbibsnet.py
@@ -7,6 +7,7 @@ from nipype.interfaces import fsl
 import nibabel as nib
 import numpy as np
 import json
+from scipy import ndimage
 
 from src.logger import LOGGER
 
@@ -243,6 +244,9 @@ def make_asegderived_mask(j_args, aseg_dir, t, nii_outfpath):
                                           "-fillh -ero -ero -ero -ero"),
                                out_file=output_mask_fpath)
         maths.run()
+
+    remove_extra_clusters_from_mask(output_mask_fpath, path_to_aseg=nii_outfpath)
+    
     return output_mask_fpath
 
 
@@ -561,3 +565,56 @@ def reverse_regn_revert_to_native(nifti_file_paths, chiral_out_dir,
                       "-interp", "nearestneighbour")
     return nifti_file_paths[f"native-T{t}"]
 
+
+def remove_extra_clusters_from_mask(path_to_mask, path_to_aseg = None):
+    '''Function that removes smaller/unconnected clusters from brain mask
+    
+    Parameters
+    ----------
+    
+    path_to_mask : str
+        Path to the binary (0/1) brain mask file to be edited.
+    path_to_aseg : str or None (default None)
+        Optional path to the corresponding aseg image. If provided,
+        the areas from small clusters (defined in mask space) will
+        be set to zero in a new copy of this image
+        
+    Returns
+    -------
+    None
+    
+    Makes new copies that replace the input mask file and optionally
+    aseg file. These new nifti images will have smaller non-brain regions
+    (defined based on the mask image) set to zero.
+    
+    '''
+    LOGGER.info("Removing outlying clusters")
+    mask_img = nib.load(path_to_mask)
+    #seg_img = nib.load(path_to_seg)
+    
+    temp_data = mask_img.get_fdata()
+    labels, nb = ndimage.label(temp_data)
+    largest_label_size = 0
+    largest_label = 0
+    for i in range(nb + 1):
+        if i == 0:
+            continue
+        label_size = np.sum(labels == i)
+        if label_size > largest_label_size:
+            largest_label_size = label_size
+            largest_label = i
+    new_mask_data = np.zeros(temp_data.shape)
+    new_mask_data[labels == largest_label] = 1
+    new_mask = nib.nifti1.Nifti1Image(new_mask_data.astype(np.uint8), affine=mask_img.affine, header=mask_img.header)
+    LOGGER.info("Saving mask without outlying clusters")
+    nib.save(new_mask, path_to_mask)
+    
+    if type(path_to_aseg) != type(None):
+        aseg_img = nib.load(path_to_aseg)
+        aseg_data = aseg_img.get_fdata()
+        aseg_data[new_mask_data != 1] = 0
+        new_aseg = nib.nifti1.Nifti1Image(aseg_data.astype(np.uint8), affine=aseg_img.affine, header=aseg_img.header)
+        LOGGER.info("Saving aseg without outlying clusters")
+        nib.save(new_aseg, path_to_aseg)
+
+    return
